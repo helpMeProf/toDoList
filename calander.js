25e4dfa51c1c7a251f3ed73d968fcdec069e204f
Dong Geun <dlehdrms978@gmail.com>
2021-01-28 22:17:59 +0900

    달력 - 달 변경 기능 추가 완료

--- calander.js
+++ calander.js
@@ -1,9 +1,35 @@
 const calender_content = document.querySelector("#right_content2");
 const calender_container = calender_content.querySelector(".calendar-container");
+const calender_header = calender_content.querySelector(".calendar-header");
 const WEEK_NUMER =7;
 const WEEK_DAY = ['일','월','화','수','목','금','토']
-let calendar = [];
+let currentDate = new Date();
 
+function clear_calendar(){
+  
+  const element = calender_container;
+  while (element.firstChild) {
+  element.removeChild(element.firstChild);
+}
+  let element2 = calender_header;
+  while (element2.firstChild) {
+    element2.removeChild(element2.firstChild);
+}
+
+}
+
+function prevMonthHandler(event){
+  clear_calendar();
+  currentDate = new Date(currentDate.getFullYear(),currentDate.getMonth()-1,currentDate.getDate());
+  draw_header(currentDate);
+  draw_calendar(currentDate);
+}
+function nextMonthHandler(event){
+  clear_calendar();
+  currentDate = new Date(currentDate.getFullYear(),currentDate.getMonth()+1,currentDate.getDate());
+  draw_header(currentDate);
+  draw_calendar(currentDate);
+}
 
 function getMonthData(day){
     thisYear =day.getFullYear();
@@ -20,9 +46,26 @@ function getMonthData(day){
     
     };
 }
+function draw_header(data){
+  const nowDate =data;
+  //헤더 그리기
+  const header = document.createElement("span");
+  const prevBtn = document.createElement("button");
+  const nextBtn = document.createElement("button");
+  header.innerText =`${nowDate.getFullYear()}년 ${nowDate.getMonth()+1}월`;
+  prevBtn.classList.add("prevBtn");
+  nextBtn.classList.add("nextBtn");
+  prevBtn.addEventListener("click",prevMonthHandler);
+  nextBtn.addEventListener("click",nextMonthHandler);
+  calender_header.appendChild(prevBtn);
+  calender_header.appendChild(header);
+  calender_header.appendChild(nextBtn);
+
+}
 function draw_calendar(data){
     const nowDate = getMonthData(data);
-    
+    const now =new Date();  
+   
     const thisMonthFirstDate = nowDate.thisMonthFirstDate;
     const thisMonthLastDate = nowDate.thisMonthLastDate;
     const prevMonth = new Date(nowDate.thisYear,
@@ -40,16 +83,16 @@ function draw_calendar(data){
       if(i===0){
         div.classList.add("past-month");
         div.classList.add("sun");
-        div.innerText=`${WEEK_DAY[i]} ${prevMonth.getDate()-thisMonthFirstDate.getDay()+i+1}`;
+        div.innerText=`${prevMonth.getDate()-thisMonthFirstDate.getDay()+i+1}`;
       }
       else if(i===6){
         div.classList.add("past-month");
         div.classList.add("sat");
-        div.innerText=`${WEEK_DAY[i]} ${prevMonth.getDate()-thisMonthFirstDate.getDay()+i+1}`;
+        div.innerText=`${prevMonth.getDate()-thisMonthFirstDate.getDay()+i+1}`;
       }
       else{
         div.classList.add("past-month");
-        div.innerText=`${WEEK_DAY[i]} ${prevMonth.getDate()-thisMonthFirstDate.getDay()+i+1}`;
+        div.innerText=`${prevMonth.getDate()-thisMonthFirstDate.getDay()+i+1}`;
       }
         calender_container.appendChild(div);
 
@@ -61,18 +104,18 @@ function draw_calendar(data){
       
         let div = document.createElement("div");
         div.classList.add("calendar-cell");
-        if(data.getDate()===i){
+        if(data.getDate()===i&&data.getFullYear()===now.getFullYear()&&data.getMonth()===now.getMonth()){
             div.classList.add("today");
-            div.innerText=`${WEEK_DAY[cellNumber%7]} ${i}`;
+            div.innerText=`${i}`;
         }else if(cellNumber%7===0){
           div.classList.add("sun");
-          div.innerText=`${WEEK_DAY[cellNumber%7]} ${i}`;
+          div.innerText=`${i}`;
         }
         else if(cellNumber%7===6){
           div.classList.add("sat");
-          div.innerText=`${WEEK_DAY[cellNumber%7]} ${i}`;
+          div.innerText=`${i}`;
         }else{
-            div.innerText=`${WEEK_DAY[cellNumber%7]} ${i}`;
+            div.innerText=`${i}`;
         }
        
           calender_container.appendChild(div);
@@ -82,21 +125,21 @@ function draw_calendar(data){
       }
       for(let i=nextMonth.getDate();cellNumber<42;i++){
       
-      
+       
         let div = document.createElement("div");
         div.classList.add("calendar-cell");
         if(cellNumber%7===0){
           div.classList.add("next-month");
           div.classList.add("sun");
-          div.innerText=`${WEEK_DAY[cellNumber%7]} ${i}`;
+          div.innerText=`${i}`;
         }
         else if(cellNumber%7===6){
           div.classList.add("next-month");
           div.classList.add("sat");
-          div.innerText=`${WEEK_DAY[cellNumber%7]} ${i}`;
+          div.innerText=`${i}`;
         }else{
             div.classList.add("next-month");
-            div.innerText=`${WEEK_DAY[cellNumber%7]} ${i}`;
+            div.innerText=`${i}`;
         }
        
           calender_container.appendChild(div);
@@ -110,6 +153,7 @@ function draw_calendar(data){
 }
 
 function init(){
+    draw_header(new Date());
     draw_calendar(new Date());
 
 }
